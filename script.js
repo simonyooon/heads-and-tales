95721b80a759b061abf6123bbe2b2458a1887b57
Glitch (hum-243-22) <none>
2022-09-17 23:26:13 +0000

    ❣️🥭 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("The script is sucessfully linked!");
+
+// const storyPopup = document.createElement("iframe");
+// storyPopup.className = "read-here";
+// document.getElementById("story-popup").after(storyPopup);
